fddf66ac5d62aa0eb248bfe956dd673902e28d98
Germans Kuzmins <gkuzmins@eisgroup.com>
2023-03-28 09:38:16 +0300

    update for more optimal solution

--- src/three_sum.rs
+++ src/three_sum.rs
@@ -1,5 +1,35 @@
 pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    return vec![vec![1, 2, 3], vec![4, 5], vec![6, 7, 8, 9]];
+    let mut answer: Vec<Vec<i32>> = Vec::new();
+    let mut nums = nums;
+    nums.sort_unstable();
+    for first in 0..nums.len() - 2 {
+        if first > 0 && nums[first] == nums[first - 1] {
+            continue;
+        }
+        let (mut second, mut third) = (first + 1, nums.len() - 1);
+        while second < third {
+            let sum = nums[first] + nums[second] + nums[third];
+            if 0 == sum {
+                answer.push(vec![nums[first], nums[second], nums[third]]);
+            }
+
+            if sum < 0 {
+                let second_value = nums[second];
+                while {
+                    second += 1;
+                    second < third && nums[second] == second_value
+                } {}
+            } else {
+                let third_value = nums[third];
+                while {
+                    third -= 1;
+                    third > second && nums[third] == third_value
+                } {}
+            }
+        }
+    }
+
+    answer
 }
 
 #[cfg(test)]
@@ -15,8 +45,8 @@ mod tests {
         let result = three_sum(nums);
 
         // Then:
-        let expectedResult: Vec<Vec<i32>> = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
-        assert_eq!(result, expectedResult);
+        let expected_result: Vec<Vec<i32>> = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        assert_eq!(result, expected_result);
     }
 
     #[test]
@@ -28,7 +58,33 @@ mod tests {
         let result = three_sum(nums);
 
         // Then:
-        let expectedResult: Vec<Vec<i32>> = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
-        assert_eq!(result, expectedResult);
+        let expected_result: Vec<Vec<i32>> = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn case3() {
+        // Given:
+        let nums = vec![0, 0, 0, 0];
+
+        // When:
+        let result = three_sum(nums);
+
+        // Then:
+        let expected_result: Vec<Vec<i32>> = vec![vec![0, 0, 0]];
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn case4() {
+        // Given:
+        let nums = vec![-2, 0, 1, 1, 2];
+
+        // When:
+        let result = three_sum(nums);
+
+        // Then:
+        let expected_result: Vec<Vec<i32>> = vec![vec![-2, 0, 2], vec![-2, 1, 1]];
+        assert_eq!(result, expected_result);
     }
 }
