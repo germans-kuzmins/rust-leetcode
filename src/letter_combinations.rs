85c38b87512d3e7ec1ba4d2f941e007aad1b95b6
Germans Kuzmins <gkuzmins@eisgroup.com>
2023-03-31 09:36:52 +0300

    add letter combination problem

---
+++ src/letter_combinations.rs
@@ -0,0 +1,83 @@
+use std::{collections::VecDeque, fmt::format};
+
+pub fn letter_combinations(digits: String) -> Vec<String> {
+    if digits.len() == 0 {
+        return vec![];
+    }
+    
+    let mut dequeue: VecDeque<String> = VecDeque::new();
+    let pad: [&str; 10] = [
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",
+    ];
+    dequeue.push_back(String::from(""));
+
+    for (index, c) in digits.char_indices() {
+        let digit = c.to_digit(10).unwrap() as usize;
+        
+        let digit_for_pad = pad[digit];
+        
+        while dequeue.front().unwrap().len() == index {
+            let element = dequeue.pop_front().unwrap();
+            for x in digit_for_pad.chars() {
+                dequeue.push_back( format!("{}{}", element, x));
+            }
+        }
+    }
+
+    return dequeue.into_iter().collect();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        // Given:
+        let digits = String::from("23");
+
+        // When:
+        let result = letter_combinations(digits);
+
+        // Then:
+        let expected_result: Vec<String> = vec![
+            String::from("ad"),
+            String::from("ae"),
+            String::from("af"),
+            String::from("bd"),
+            String::from("be"),
+            String::from("bf"),
+            String::from("cd"),
+            String::from("ce"),
+            String::from("cf"),
+        ];
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn case2() {
+        // Given:
+        let digits = String::from("");
+
+        // When:
+        let result = letter_combinations(digits);
+
+        // Then:
+        let expected_result: Vec<String> = vec![];
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn case3() {
+        // Given:
+        let digits = String::from("2");
+
+        // When:
+        let result = letter_combinations(digits);
+
+        // Then:
+        let expected_result: Vec<String> =
+            vec![String::from("a"), String::from("b"), String::from("c")];
+        assert_eq!(result, expected_result);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod container_with_most_water;
 pub mod roman_to_int;
 pub mod longest_common_prefix;
 pub mod three_sum;
+pub mod letter_combinations;
