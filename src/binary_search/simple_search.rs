410da11a93ea2839478715c188a09dcf37effd2f
Germans Kuzmins <gkuzmins@eisgroup.com>
2023-07-11 15:10:13 +0300

    added first binary search problem

---
+++ src/binary_search/simple_search.rs
@@ -0,0 +1,42 @@
+pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+    let (mut l, mut r) = (0, nums.len());
+
+    while l < r {
+        let m = l + (r - l) / 2;
+        match target.cmp(&nums[m]) {
+            std::cmp::Ordering::Equal => return m as i32,
+            std::cmp::Ordering::Less => r = m,
+            std::cmp::Ordering::Greater => l = m + 1,
+        }
+    }
+
+    return -1;
+}
+
+mod test {
+    #[test]
+    fn case1() {
+        // Given:
+        let nums = vec![-1, 0, 3, 5, 9, 12];
+        let target = 9;
+
+        // When:
+        let result = super::search(nums, target);
+
+        // Then:
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn case2() {
+        // Given:
+        let nums = vec![-1, 0, 3, 5, 9, 12];
+        let target = 2;
+
+        // When:
+        let result = super::search(nums, target);
+
+        // Then:
+        assert_eq!(result, -1);
+    }
+}
